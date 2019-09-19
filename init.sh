9aeb2350a952629a23243f94b691a907f63b3789
Felix Haase <haase-felix@gmx.de>
2019-09-19 20:18:05 +0200

    Add File Environment variable replacement

--- init.sh
+++ init.sh
@@ -1,22 +1,40 @@
 #!/bin/bash -e
-set -e
 
 if [ ! -e "$FIRST_START_DONE" ]; then
 
-    CN_ADMIN="cn=admin,ou=aclroles,${LDAP_BASE_DN}"
-    UID_FD_ADMIN="uid=${FD_ADMIN_USERNAME},${LDAP_BASE_DN}"
-    CN_ADMIN_BS64=$(echo -n ${CN_ADMIN} | base64 | tr -d '\n')
-    UID_FD_ADMIN_BS64=$(echo -n ${UID_FD_ADMIN} | base64 | tr -d '\n')
+	function ldap_add_or_modify (){
+    local LDIF_FILE=$1
+		
+    log-helper debug "Processing file ${LDIF_FILE}"
+    sed -i "s|{{ LDAP_BASE_DN }}|${LDAP_BASE_DN}|g" $LDIF_FILE
+    sed -i "s|{{ LDAP_BACKEND }}|${LDAP_BACKEND}|g" $LDIF_FILE
+    sed -i "s|{{ LDAP_DOMAIN }}|${LDAP_DOMAIN}|g" $LDIF_FILE
+    sed -i "s|{{ CN_ADMIN_BS64 }}|${CN_ADMIN_BS64}|g" $LDIF_FILE
+    sed -i "s|{{ UID_FD_ADMIN_BS64 }}|${FD_ADMIN_PASSWORD}|g" $LDIF_FILE
+    sed -i "s|{{ FD_ADMIN_PASSWORD }}|${FD_ADMIN_PASSWORD}|g" $LDIF_FILE
+    if grep -iq changetype $LDIF_FILE ; then
+        ( ldapmodify -Y EXTERNAL -Q -H ldapi:/// -f $LDIF_FILE 2>&1 || ldapmodify -h localhost -p 389 -D cn=admin,$LDAP_BASE_DN -w "$LDAP_ADMIN_PASSWORD" -f $LDIF_FILE 2>&1 ) | log-helper debug
+    else
+        ( ldapadd -Y EXTERNAL -Q -H ldapi:/// -f $LDIF_FILE 2>&1 || ldapadd -h localhost -p 389 -D cn=admin,$LDAP_BASE_DN -w "$LDAP_ADMIN_PASSWORD" -f $LDIF_FILE 2>&1 ) | log-helper debug
+    fi
+  }
 
-    LDAP_ADMIN_PASSWORD_HASH=$(slappasswd -s $LDAP_ADMIN_PASSWORD)
-    FD_ADMIN_PASSWORD_HASH=$(slappasswd -s $FD_ADMIN_PASSWORD)
+	CN_ADMIN="cn=admin,ou=aclroles,${LDAP_BASE_DN}"
+	UID_FD_ADMIN="uid=${FD_ADMIN_USERNAME},${LDAP_BASE_DN}"
+	CN_ADMIN_BS64=$(echo -n ${CN_ADMIN} | base64 | tr -d '\n')
+	UID_FD_ADMIN_BS64=$(echo -n ${UID_FD_ADMIN} | base64 | tr -d '\n')
 
-    fusiondirectory-insert-schema
-    fusiondirectory-insert-schema -i /etc/ldap/schema/fusiondirectory/*.schema
-    fusiondirectory-insert-schema -m /etc/ldap/schema/fusiondirectory/modify/*.schema
+	LDAP_ADMIN_PASSWORD_HASH=$(slappasswd -s $LDAP_ADMIN_PASSWORD)
+	FD_ADMIN_PASSWORD_HASH=$(slappasswd -s $FD_ADMIN_PASSWORD)
 
-    ldapmodify -x -D "cn=admin,${LDAP_BASE_DN}" -w ${LDAP_ADMIN_PASSWORD} -f /var/fusiondirectory/bootstrap/ldif/modify.ldif
-    ldapadd -x -D "cn=admin,${LDAP_BASE_DN}" -w ${LDAP_ADMIN_PASSWORD} -f /var/fusiondirectory/bootstrap/ldif/add.ldif
+	fusiondirectory-insert-schema
+	mkdir /etc/ldap/schema/fusiondirectory/modify/
+	mv /etc/ldap/schema/fusiondirectory/rfc2307bis.schema /etc/ldap/schema/fusiondirectory/modify/rfc2307bis.schema
+	fusiondirectory-insert-schema -i /etc/ldap/schema/fusiondirectory/*.schema
+	fusiondirectory-insert-schema -m /etc/ldap/schema/fusiondirectory/modify/*.schema
 
-    rm -rf /tmp/*
+	ldap_add_or_modify "/var/fusiondirectory/bootstrap/ldif/modify.ldif"
+	ldap_add_or_modify "/var/fusiondirectory/bootstrap/ldif/add.ldif"
+
+	rm -rf /tmp/*
 fi
